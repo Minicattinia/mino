ba0cb3b89945175a9bde5dbef78bd09649379e71
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-12-16 20:19:32 +0700

    Update miner-avian-minotaurx.bat

--- miner-avian-minotaurx.bat
+++ miner-avian-minotaurx.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RRb6iaoP4YxQCZA2vjqjCT7WtB1QAA1TgW -p m=solo
+"%~dp0"cpuminer-sse2.exe -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RRb6iaoP4YxQCZA2vjqjCT7WtB1QAA1TgW.meet
 goto start
